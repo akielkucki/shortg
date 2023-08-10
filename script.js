91d81643d4d10b55f4197171feefe44887a873a6
akielkucki <123397448+akielkucki@users.noreply.github.com>
2023-08-10 11:19:03 -0400

    Update script.js

---
+++ script.js
@@ -0,0 +1,29 @@
+
+var audio = document.getElementById("audio");
+document.body.addEventListener("mousemove", function () {
+    audio.play()
+})
+const videoContainer = document.getElementById('video-container');
+const videoElement = document.getElementById('resizable-video');
+
+function resizeVideo() {
+    const containerWidth = videoContainer.clientWidth;
+    const containerHeight = videoContainer.clientHeight;
+    const originalAspectRatio = videoElement.videoWidth / videoElement.videoHeight;
+
+    // Calculate new height to maintain the original aspect ratio
+    const newHeight = containerWidth / originalAspectRatio;
+    const newWidth = containerHeight / originalAspectRatio;
+
+    videoElement.style.height = `${newHeight}px`;
+    if (window.innerWidth <= 800) {
+   
+        videoElement.style.height = `100vh`;
+    }
+}
+
+// Initial resize and attach the event listener
+resizeVideo();
+window.addEventListener('resize', resizeVideo);
+
+  
